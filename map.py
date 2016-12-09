3c26aef4bef97ac48d7626936ebc4b2cb5b14eea
Janet Zhang <jzhang12@stuy.edu>
2016-12-09 12:36:49 -0500

    dict

--- map.py
+++ map.py
@@ -1,4 +1,16 @@
 import plotly.plotly as py
+
+
+import csv
+reader = csv.DictReader(open('2011_us_ag_exports.csv'))
+
+result = {}
+for row in reader:
+    for column, value in row.iteritems():
+        result.setdefault(column, []).append(value)
+#print result
+
+'''
 import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
@@ -8,20 +20,31 @@ for col in df.columns:
 
 scl = [[0.0, 'rgb(242,240,247)'],[0.2, 'rgb(218,218,235)'],[0.4, 'rgb(188,189,220)'],\
             [0.6, 'rgb(158,154,200)'],[0.8, 'rgb(117,107,177)'],[1.0, 'rgb(84,39,143)']]
+'''
+df = result
+x = {'text':[]}
+s=""
+while x < len( df ['beef'] ):
+		s += '<br>' +\
+    'Beef '+df[x]['beef']+' Dairy '+df[x]['dairy']+'<br>'+\
+    'Fruits '+df[x]['total fruits']+' Veggies ' + df[x]['total veggies']+'<br>'+\
+    'Wheat '+df[x]['wheat']+' Corn '+df[x]['corn']
 
-df['text'] = df['state'] + '<br>' +\
-    'Beef '+df['beef']+' Dairy '+df['dairy']+'<br>'+\
-    'Fruits '+df['total fruits']+' Veggies ' + df['total veggies']+'<br>'+\
-    'Wheat '+df['wheat']+' Corn '+df['corn']
+
+df = result
+
+scl = [[0.0, 'rgb(242,240,247)'],[0.2, 'rgb(218,218,235)'],[0.4, 'rgb(188,189,220)'],\
+            [0.6, 'rgb(158,154,200)'],[0.8, 'rgb(117,107,177)'],[1.0, 'rgb(84,39,143)']]
+
 
 data = [ dict(
         type='choropleth',
         colorscale = scl,
         autocolorscale = False,
         locations = df['code'],
-        z = df['total exports'].astype(float),
+        z = df['total exports'],#.astype(float),
         locationmode = 'USA-states',
-        text = df['text'],
+        text = x['text'],
         marker = dict(
             line = dict (
                 color = 'rgb(255,255,255)',
--- pandas.py
+++ pandas.py
@@ -1,36 +1,3 @@
-'''
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
-
-for col in df.columns:
-    df[col] = df[col].astype(str)
-  '''  
-    #replace using csv reader
-    
-import csv
-'''with open ('2011_us_ag_exports.csv', 'r+') as csvfile:
-	fieldnames = ['code','state','category','total exports','beef','pork','poultry','dairy,fruits fresh','fruits proc','total fruits','veggies fresh','veggies proc','total veggies','corn','wheat','cotton']
-	#reader
-	reader = csv.DictReader(csvfile)
-	#with open ('2011_us_ag_exports.csv', 'w') as csvfile:
-	writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-	writer.writeheader()
-	for row in reader:
-		writer.writerow({'code': row['code']})
-		print row['code']
-		'''
-
-#reader = csv.reader(open('2011_us_ag_exports.csv'))
-
-'''
-result = {}
-for row in reader:
-    key = row[0]
-    if key in result:
-        # implement your duplicate row handling here
-        pass
-    result[key] = row[1:]
-print result
-'''
 import csv
 reader = csv.DictReader(open('2011_us_ag_exports.csv'))
 
